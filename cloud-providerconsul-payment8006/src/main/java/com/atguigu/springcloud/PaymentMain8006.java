ad17cc0e1352adbfa1cfd27f654f8c5227ea31c7
VoaNien <oyy94@163.com>
2021-01-03 23:23:04 +0800

    注册 consul

---
+++ cloud-providerconsul-payment8006/src/main/java/com/atguigu/springcloud/PaymentMain8006.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+  * 
+  * @return 
+  * @throws 
+  * @Author OuYang Yi
+  * @2020/12/31
+ */
+@SpringBootApplication
+public class PaymentMain8006 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8006.class,args);
+    }
+}
