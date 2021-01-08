979e5b8b71c281f01e7f6f2823cae44d3e34098d
ouyangyi <oyy994@163.com>
2021-01-08 18:52:43 +0800

    gateway9527

---
+++ cloud-gateway-gateway9527/src/test/java/T3.java
@@ -0,0 +1,7 @@
+import java.time.ZonedDateTime;
+
+public class T3 {
+    public static void main(String[] args) {
+        ZonedDateTime zonedDateTime= ZonedDateTime.now();
+        System.out.println(zonedDateTime);
+    }}
